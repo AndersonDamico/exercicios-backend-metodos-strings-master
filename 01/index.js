eefe7cfdaa229c0d42329e96fda1e7871914afef
AndersonDamico <andersondamico2@gmail.com>
2023-07-11 15:27:05 -0300

    Exercicio 01

--- 01/index.js
+++ 01/index.js
@@ -1,1 +1,10 @@
 const comentario = "Esse COVID é muito perigoso!";
+
+const achouCovid = comentario.toLowerCase().includes('covid');
+const achouPanemia = comentario.toLowerCase().includes('pandemia');
+
+if (achouCovid || achouPanemia) {
+    console.log('comentario bloqueado por conter palavras proibidas');
+} else {
+    console.log('comentario autorizado');
+}
