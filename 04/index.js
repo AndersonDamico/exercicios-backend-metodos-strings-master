2fd37464513a1fcd5102cb297b4a67908370661b
AndersonDamico <andersondamico2@gmail.com>
2023-07-11 16:41:20 -0300

    Exercicio 04

--- 04/index.js
+++ 04/index.js
@@ -1,3 +1,17 @@
 let identificador = "123";
 let nome = "José silva costa";
 let email = "      jose@email.com  ";
+
+identificador = identificador.padStart(6,'0');
+
+let nomeMaiusculo = '';
+
+for (const item of nome.split('')) {
+    nomeMaiusculo += `${item.slice(0, 1).toUpperCase()}${item.slice(1).toLowerCase()} `
+}
+nome = nomeMaiusculo.trim(); //remove o spaco no inicio e no final da string
+email = email.trim();
+
+console.log(identificador)
+console.log(nome)
+console.log(email)
